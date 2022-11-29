540b143b0c160ead16be39c0e4557ba3d39afbfc
Rodrigo Guerrero <rodrigo.guerrero@solera.com>
2022-11-29 16:56:38 +0100

    Add new class with methods that run in a specific order

---
+++ src/test/java/com/guerrero/MethodsOrderedByOrderIndexTest.java
@@ -0,0 +1,35 @@
+package com.guerrero;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MethodsOrderedByOrderIndexTest {
+
+    @Test
+    @Order(3)
+    void testD() {
+        System.out.println("Running test D");
+    }
+
+    @Test
+    @Order(2)
+    void testA() {
+        System.out.println("Running test A");
+    }
+
+    @Test
+    @Order(4)
+    void testC() {
+        System.out.println("Running test C");
+    }
+
+    @Test
+    @Order(1)
+    void testB() {
+        System.out.println("Running test B");
+    }
+
+}
