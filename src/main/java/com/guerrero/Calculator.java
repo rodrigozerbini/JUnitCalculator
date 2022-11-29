556075b6221f8ecc8856925a440f151ef991a64f
Rodrigo Guerrero <rodrigo.guerrero@solera.com>
2022-11-29 15:35:35 +0100

    Add BeforeAll, AfterAll, BeforeEach, AfterEach methods

--- src/main/java/com/guerrero/Calculator.java
+++ src/main/java/com/guerrero/Calculator.java
@@ -2,7 +2,11 @@ package com.guerrero;
 
 public class Calculator {
 
-    public int integerDivision(int dividend, int divisor) {
-        return dividend/divisor;
+    public int divideIntegers(int dividend, int divisor) throws ArithmeticException {
+        return dividend / divisor;
+    }
+
+    public int subtractIntegers(int a, int b) {
+        return a - b;
     }
 }
--- src/test/java/com/guerrero/CalculatorTest.java
+++ src/test/java/com/guerrero/CalculatorTest.java
@@ -2,11 +2,76 @@ package com.guerrero;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Test Math operations in Calculator")
 class CalculatorTest {
 
+    Calculator calculator;
+
+    @BeforeAll
+    static void setup() {
+        System.out.println("Executing @BeforeAll method");
+    }
+
+    @AfterAll
+    static void cleanup() {
+        System.out.println("Executing @AfterAll method");
+    }
+
+    @BeforeEach
+    void beforeEachMethod() {
+        System.out.println("Executing @BeforeEach method");
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    void afterEachMethod() {
+        System.out.println("Executing @AfterEach method");
+    }
+
+    @DisplayName("Test 4/2 = 2")
     @Test
-    void integerDivision() {
+    void testDivideIntegers_WhenFourIsDividedByTwo_ShouldReturnTwo() {
+        // Act
+        int result = calculator.divideIntegers(4,2);
+
+        // Assert
+        assertEquals(2, result, "4/2 should be 2");
+    }
+
+    @DisplayName("Test 10/2 = 5")
+    @Test
+    void testDivideIntegers_WhenTenIsDividedByTwo_ShouldReturnFive() {
+        int result = calculator.divideIntegers(10,2);
+
+        assertEquals(5, result, "10/2 should be 5");
+    }
+
+    @DisplayName("Test division by zero")
+
+    @Test
+    void testDivideIntegers_WhenIsDividedByZero_ShouldThrowArithmeticException() {
+
+        // Act & Assert
+        assertThrows(ArithmeticException.class, ()-> {calculator.divideIntegers(5, 0);}, "5/0 should return an exception");
+    }
+
+    @DisplayName("Test 10 - 5 = 5")
+    @Test
+    void testSubtractIntegers_WhenTenIsSubtractedByFive_ShouldReturnFive() {
+        int a = 10;
+        int b = 5;
+        int expectedResult = 5;
+        int result = calculator.subtractIntegers(a, b);
+
+        // The lambda functions will be executed only if the test fails
+        // This optimizes performance
+        assertEquals(expectedResult, result, () -> a + "-" + b + " should be " + expectedResult);
     }
 }
