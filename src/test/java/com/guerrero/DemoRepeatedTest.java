6af58062b027a790a6305fce2110c783b7fe9d3f
Rodrigo Guerrero <rodrigo.guerrero@solera.com>
2022-11-29 16:52:20 +0100

    Add new class with methods that run in a MethodName order

--- src/test/java/com/guerrero/DemoRepeatedTest.java
+++ src/test/java/com/guerrero/DemoRepeatedTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 
 public class DemoRepeatedTest {
 
--- src/test/java/com/guerrero/MethodsOrderedRandomlyTest.java
+++ src/test/java/com/guerrero/MethodsOrderedRandomlyTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder(MethodOrderer.Random.class)
-public class MethodOrderedRandomlyTest {
+public class MethodsOrderedRandomlyTest {
 
     @Test
     void testA() {
