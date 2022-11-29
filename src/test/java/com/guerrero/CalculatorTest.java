446771519c88cecf354abb3ffd6a20a8dd6d811e
Rodrigo Guerrero <rodrigo.guerrero@solera.com>
2022-11-29 13:52:02 +0100

    Add Calculator class and first test

---
+++ src/test/java/com/guerrero/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.guerrero;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void integerDivision() {
+    }
+}
